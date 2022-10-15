363c416e4e503041cdefd6934ff8cd22767dc145
George Rolston <grolston@outlook.com>
2022-10-15 07:25:11 -0600

    function updates

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+//Add an import to use the API and Auth components from aws-amplify
+import { API, Auth } from 'aws-amplify'
 
 function App({ signOut }) {
   return (
@@ -19,6 +21,19 @@ function App({ signOut }) {
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
+}
+
+//modify the useEffect() function to include the checkUser() function call
+useEffect(() => {
+  fetchPosts();
+  checkUser(); // new function call
+});
+
+//define the checkUser function after the existing fetchPosts() function
+async function checkUser() {
+  const user = await Auth.currentAuthenticatedUser();
+  console.log('user:', user);
+  console.log('user attributes: ', user.attributes);
 }
 
 export default withAuthenticator(App);
